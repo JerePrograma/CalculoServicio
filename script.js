7c2a4913c2e101e9cea375c655ff8ea79c39e5c3
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2023-07-21 19:35:15 -0300

    Versión 1.1

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+  });
+
+  calendar.render();
+
+  document.getElementById("calcular").addEventListener("click", function () {
+    let horario = document.getElementById("horario").value;
+    let fechaInicio = new Date(document.getElementById("fecha").value);
+
+    fechaInicio.setUTCHours(0, 0, 0, 0);
+
+    calendar.getEvents().forEach(function (event) {
+      event.remove();
+    });
+
+    let incremento;
+    if (horario === "12x36") {
+      incremento = 2;
+    } else if (horario === "24x48") {
+      incremento = 3;
+    }
+
+    let fechaActual = new Date(fechaInicio);
+    let finalYear = new Date(fechaInicio.getUTCFullYear(), 11, 31);
+
+    while (fechaActual <= finalYear) {
+      let fechaString = formatDate(fechaActual);
+      calendar.addEvent({
+        title: "Servicio",
+        start: fechaString,
+        allDay: true,
+      });
+
+      fechaActual.setUTCDate(fechaActual.getUTCDate() + incremento);
+    }
+
+    calendar.gotoDate(formatDate(fechaInicio));
+  });
+
+  function formatDate(date) {
+    var d = new Date(date),
+      month = "" + (d.getUTCMonth() + 1),
+      day = "" + d.getUTCDate(),
+      year = d.getUTCFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+  }
+});
