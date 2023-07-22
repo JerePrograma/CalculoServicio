15a53da093fa4e848a95a24462972dcabed28116
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2023-07-22 16:08:25 -0300

    cambio modalidad 48x96

--- script.js
+++ script.js
@@ -11,38 +11,51 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("calcular").addEventListener("click", function () {
     let horario = document.getElementById("horario").value;
     let fechaInicio = new Date(document.getElementById("fecha").value);
-
+  
     fechaInicio.setUTCHours(0, 0, 0, 0);
-
+  
     calendar.getEvents().forEach(function (event) {
       event.remove();
     });
-
+  
     let incremento;
     if (horario === "12x36") {
       incremento = 2;
     } else if (horario === "24x48") {
       incremento = 3;
-    } else if (horario === "48x96") { // Añade este condicional
-      incremento = 5;
+    } else if (horario === "48x96") {
+      incremento = 6;  // Aquí es donde se cambia el incremento de 5 a 6
     }
-
+  
     let fechaActual = new Date(fechaInicio);
     let finalYear = new Date(fechaInicio.getUTCFullYear(), 11, 31);
-
+  
     while (fechaActual <= finalYear) {
-      let fechaString = formatDate(fechaActual);
-      calendar.addEvent({
-        title: "Servicio",
-        start: fechaString,
-        allDay: true,
-      });
-
-      fechaActual.setUTCDate(fechaActual.getUTCDate() + incremento);
+      if (horario === "48x96") {
+        for (let i = 0; i < 2; i++) { // Añadir 2 días de trabajo
+          let fechaString = formatDate(new Date(fechaActual));
+          calendar.addEvent({
+            title: "Servicio",
+            start: fechaString,
+            allDay: true,
+          });
+          fechaActual.setUTCDate(fechaActual.getUTCDate() + 1);
+        }
+        fechaActual.setUTCDate(fechaActual.getUTCDate() + 4); // Añadir 4 días de descanso
+      } else {
+        let fechaString = formatDate(fechaActual);
+        calendar.addEvent({
+          title: "Servicio",
+          start: fechaString,
+          allDay: true,
+        });
+        fechaActual.setUTCDate(fechaActual.getUTCDate() + incremento);
+      }
+  
     }
-
     calendar.gotoDate(formatDate(fechaInicio));
   });
+  
 
 
   function formatDate(date) {
